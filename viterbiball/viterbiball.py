956cb4f821ca31125d1e59abf32127da48310eed
liangvic <shirosaccount@gmail.com>
2014-03-08 21:45:56 -0800

    reverted to e8e67b971563cc078cc65865ca02469a927ac923

--- viterbiball/viterbiball.py
+++ viterbiball/viterbiball.py
@@ -11,9 +11,9 @@ import webapp2
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-	extensions=['jinja2.ext.autoescape'],
-	autoescape=True)
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 
 DEFAULT_SALESBOOK_NAME = 'default_salesbook'
@@ -24,93 +24,68 @@ DEFAULT_SALESBOOK_NAME = 'default_salesbook'
 # However, the write rate should be limited to ~1/second.
 
 def salesbook_key(salesbook_name=DEFAULT_SALESBOOK_NAME):
-	"""Constructs a Datastore key for a Salesbook entity with salesbook_name."""
-	return ndb.Key('Salesbook', salesbook_name)
-	self.response.write('Hello world!')
+    """Constructs a Datastore key for a Salesbook entity with salesbook_name."""
+    return ndb.Key('Salesbook', salesbook_name)
 
 
 class Student(ndb.Model):
-<<<<<<< HEAD
-	#"Models an individual Salesbook entry with email, name, and date."
-	email = ndb.UserProperty()
-	name = ndb.StringProperty(indexed=False)
-	uscid = ndb.StringProperty(indexed=False)
-	major = ndb.StringProperty(indexed=False)
-	year = ndb.StringProperty(indexed=False)
-	date = ndb.DateTimeProperty(auto_now_add=True)
-=======
     """Models an individual Salesbook entry with email, name, and date."""
     email = ndb.StringProperty(indexed=False)
     name = ndb.StringProperty(indexed=False)
     uscid = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
->>>>>>> e8e67b971563cc078cc65865ca02469a927ac923
 
 
 
 class MainPage(webapp2.RequestHandler):
 
-	def get(self):
-		salesbook_name = self.request.get('salesbook_name',
+    def get(self):
+        salesbook_name = self.request.get('salesbook_name',
                                           DEFAULT_SALESBOOK_NAME)
-		students_query = Student.query(
-			ancestor=salesbook_key(salesbook_name)).order(-Student.date)
-			students = students_query.fetch(10)
+        students_query = Student.query(
+            ancestor=salesbook_key(salesbook_name)).order(-Student.date)
+        students = students_query.fetch(10)
 
-		if users.get_current_user():
-			url = users.create_logout_url(self.request.uri)
-			url_linktext = 'Logout'
-		else:
-			url = users.create_login_url(self.request.uri)
-			url_linktext = 'Login'
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
 
-		template_values = {
-			'students': students,
-			'salesbook_name': urllib.quote_plus(salesbook_name),
-			'url': url,
-			'url_linktext': url_linktext,
-		}
+        template_values = {
+            'students': students,
+            'salesbook_name': urllib.quote_plus(salesbook_name),
+            'url': url,
+            'url_linktext': url_linktext,
+        }
 
-		template = JINJA_ENVIRONMENT.get_template('frontpage.html')
-		self.response.write(template.render(template_values))
+        template = JINJA_ENVIRONMENT.get_template('frontpage.html')
+        self.response.write(template.render(template_values))
 
 
 
 class Salesbook(webapp2.RequestHandler):
 
-	def post(self):
-		# We set the same parent key on the 'Student' to ensure each Student
-		# is in the same entity group. Queries across the single entity group
-		# will be consistent. However, the write rate to a single entity group
-		# should be limited to ~1/second.
-		salesbook_name = self.request.get('salesbook_name',
+    def post(self):
+        # We set the same parent key on the 'Student' to ensure each Student
+        # is in the same entity group. Queries across the single entity group
+        # will be consistent. However, the write rate to a single entity group
+        # should be limited to ~1/second.
+        salesbook_name = self.request.get('salesbook_name',
                                           DEFAULT_SALESBOOK_NAME)
-<<<<<<< HEAD
-		student = Student(parent=salesbook_key(salesbook_name))
-
-		if users.get_current_user():
-			student.email = users.get_current_user()
-
-		student.name = self.request.get('name')
-		student.uscid = self.request.get('uscid')
-		student.major = self.request.get('major')
-		student.year = self.request.get('year')
-		student.put()
-=======
         student = Student(parent=salesbook_key(salesbook_name))
 			
-		student.email = self.request.get('email')
+		#student.email = self.request.get('email')
         student.name = self.request.get('name')
         student.uscid = self.request.get('id')
         student.put()
->>>>>>> e8e67b971563cc078cc65865ca02469a927ac923
 
-		query_params = {'salesbook_name': salesbook_name}
-		self.redirect('/?' + urllib.urlencode(query_params))
+        query_params = {'salesbook_name': salesbook_name}
+        self.redirect('/?' + urllib.urlencode(query_params))
 
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/register', Salesbook),
->>>>>>> e8e67b971563cc078cc65865ca02469a927ac923
 ], debug=True)
