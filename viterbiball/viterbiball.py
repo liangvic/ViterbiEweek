e8a54c8bd5e9ce4f3db3381b491e5906a8c8bfbf
Demi Nguyen <deminguy@usc.edu>
2014-03-08 16:45:46 -0800

    moved folders

--- viterbiball/viterbiball.py
+++ viterbiball/viterbiball.py
@@ -61,7 +61,7 @@ class MainPage(webapp2.RequestHandler):
             'url_linktext': url_linktext,
         }
 
-        template = JINJA_ENVIRONMENT.get_template('index.html')
+        template = JINJA_ENVIRONMENT.get_template('frontpage.html')
         self.response.write(template.render(template_values))
 
 
